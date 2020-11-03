6f9ce57cd0bee39fc8f75c74f6b2c25fb5a099f7
최진혁 <chlwlsgur96@hotmail.com>
2020-11-03 23:22:41 +0900

    * IoTModule *
    - Update BTCommand.h

--- BTCommunication/BTCommunication.h
+++
@@ -1,7 +0,0 @@
-#include <SoftwareSerial.h>
-
-SoftwareSerial btSerial(2, 3);
-
-void InitializeBT();
-void SendResponse(String response);
-String GetRequest();
---
+++ IoTModule/BTCommunication/BTCommand.h
@@ -0,0 +1,32 @@
+// Check Response
+#define CHECK_RESPONSE "Server"
+#define SEND_RESPONSE "Module"
+
+// Common
+#define REQUEST_TYPE "Type"
+
+// Type
+#define TYPE_HT "HT"
+#define TYPE_DUST "Dust"
+#define TYPE_Light "Light"
+#define TYPE_CCTV "CCTV"
+#define TYPE_GAS "Gas"
+#define TYPE_DOORLOCK "Doorlock"
+
+// HT
+#define REQUEST_HT_HUMIDITY "HT_Humidity"
+#define REQUEST_HT_TEMP "HT_Temp"
+
+// Dust Detector
+#define REQUEST_DUST_DUST "Dust_Dust"
+#define REQUEST_DUST_LEVEL "Dust_Level"
+
+// Light
+#define REQUEST_LIGHT_VALUE "Light_Value"
+#define REQUEST_LIGHT_LEVEL "Light_Level"
+
+// CCTV
+
+// Gas
+
+// Doorlock
